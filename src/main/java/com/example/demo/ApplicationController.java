3f3237b1d475f79b8306cff8983a6d97d077e5dc
Kamil Olech <kamilolech07@gmail.com>
2022-04-27 16:55:24 +0100

    html home

---
+++ src/main/java/com/example/demo/ApplicationController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+	@Autowired
+	private myRepo repo;
+	
+	@GetMapping("")
+	public String viewHome()
+	{
+		return "home";
+	}
+}
