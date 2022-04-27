aaf6359541fd41cc6acd611410747031170ae796
Kamil Olech <kamilolech07@gmail.com>
2022-04-27 15:31:00 +0100

    works

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+//@Entity
+//@Table("cars")
+
+public class Car {
+	
+	
+
+	private int id;
+	private String make;
+	private String car_model;
+	private String problem;
+}
