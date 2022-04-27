56bfbdd243672ac5eca4b700892a3115efd22c53
Kamil Olech <kamilolech07@gmail.com>
2022-04-27 18:07:44 +0100

    POST in progress

--- src/main/java/com/example/demo/ApplicationController.java
+++ src/main/java/com/example/demo/ApplicationController.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ApplicationController {
@@ -13,5 +16,11 @@ public class ApplicationController {
 	public String viewHome()
 	{
 		return "home";
+	}
+	@PostMapping("")
+	public String sendData(@ModelAttribute Car car, Model model)
+	{
+		model.addAttribute("car", new Car());
+		return "result";
 	}
 }
